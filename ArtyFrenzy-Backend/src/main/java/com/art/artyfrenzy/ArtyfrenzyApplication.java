79dc5b92508fa788a9eb81b5255dcdb91b3513a4
PrajnaNyk <nayakprajna42@gmail.com>
2026-05-01 10:30:38 +0530

    Initialize backend with Maven setup and Spring Boot application

---
+++ ArtyFrenzy-Backend/src/main/java/com/art/artyfrenzy/ArtyfrenzyApplication.java
@@ -0,0 +1,13 @@
+package com.art.artyfrenzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtyfrenzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtyfrenzyApplication.class, args);
+	}
+
+}
---
+++ ArtyFrenzy-Backend/src/test/java/com/art/artyfrenzy/ArtyfrenzyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.art.artyfrenzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtyfrenzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
